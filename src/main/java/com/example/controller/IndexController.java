93300339d10422570ad999b8dbb1c0fe12384ea1
gaoxiang <1130045113@qq.com>
2020-06-02 13:05:41 +0800

    新建好项目

---
+++ src/main/java/com/example/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+/**
+ * @ClassName IndexController
+ * @Description: TODO
+ * @Author 有丶优秀的少年
+ * @Date 2020/6/2
+ * @Version V1.0
+ **/
+public class IndexController {
+}
